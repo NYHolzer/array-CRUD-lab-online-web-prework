2fcf4e7c0ef65619b599a3c2a08e78d1b3b9bec3
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-08 23:55:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  bros = ["Aton", "Ariel", "Julie", "Rachel"]
-  bros << "Eli"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
